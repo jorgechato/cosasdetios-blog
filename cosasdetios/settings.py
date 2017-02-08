a8e7de3cbb8df2f6830f98674e65404e1e646d1b
orggue <jorgechato1@gmail.com>
2017-02-08 16:22:30 +0100

    update cover

--- cosasdetios/settings.py
+++ cosasdetios/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 THUMBNAIL_DEBUG = DEBUG
 
 ALLOWED_HOSTS = [
